ea2c57fb1545a885caa125977f345b398941033a
Merve Nur <mervenurktlmss@gmail.com>
2023-02-02 23:31:04 +0300

    diğer testlere geçiş yapıldı

---
+++ question.js
@@ -0,0 +1,64 @@
+const data = [
+
+
+    {
+        id: 1,
+        soru: "En Sevdiğin Müzisyen?",
+        cevaplar: {
+                a: "Hakkı Bulut",
+                b: "Lana Del Rey",
+                c: "Mabel Matiz",
+                d: "Linkin Park",
+        }
+    },
+    {
+        id: 2,
+        soru: "atlar uçar mıqweqe?",
+        cevaplar: {
+                a: "cevap a",
+                b: "cevap b",
+                c: "cevap c",
+                d: "cevap d",
+        }
+    },
+    {
+        id: 3,
+        soru: "En Sevdiğin 45Müzisyen?",
+        cevaplar: {
+                a: "Sezen Aksu",
+                b: "İbrahim Tatlıses",
+                c: "Ahmet Kaya",
+                d: "Ferdi Tayfur",
+        }
+    },
+    {
+        id: 4,
+        soru: "En Sevdiğin45 Müzisyen?",
+        cevaplar: {
+                a: "Hakkı Bulut",
+                b: "Lana Del Rey",
+                c: "Mabel Matiz",
+                d: "Linkin Park",
+        }
+    },
+    {
+        id: 5,
+        soru: "En Sevdiğin Müzisye45n?",
+        cevaplar: {
+                a: "Hakkı Bulut",
+                b: "Lana Del Rey",
+                c: "Mabel Matiz",
+                d: "Linkin Park",
+        }
+    },
+    {
+        id: 6,
+        soru: "En Sevdiğin Müzisyen4545545?",
+        cevaplar: {
+                a: "Hakkı Bulut",
+                b: "Lana Del Rey",
+                c: "Mabel Matiz",
+                d: "Linkin Park",
+        }
+    },
+];
--- script.js
+++ script.js
@@ -1,23 +1,102 @@
+const result = [
+    {isim: "Yeliz", resim:"media/yeliz.jpg"},
+    {isim: "Yaren", resim:"media/yaren.webp"},
+    {isim: "Aybüke", resim:"media/aybuke.webp"},
+    {isim: "Ayşenur", resim:"media/aysenur.webp"},
+    {isim: "Şeydanur", resim:"media/seydanur.webp"},
+    {isim: "Cansel", resim:"media/cansel.jpg"},
+    {isim: "Tuğçe", resim:"media/tugce.jpg"},
+    {isim: "Elif", resim:"media/elif.jpg"},
+    {isim: "Simge", resim:"media/sımge.webp"}
 
-// let siradaki = 0;
+]
+const container = document.getElementById("question-container");
+const background = document.querySelector(".background-filter");
+const kismet = document.getElementById("kismet");
+const aski = document.getElementById("aski");
+const harryPotter = document.getElementById("harryPotter");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const closeButton = document.querySelector(".close_btn");
+const finishBtn = document.createElement("button");
+const submitButton = document.querySelector(".submit");
 
-// img.addEventListener('click', () => {
-//     div.classList.add("testOpen");
-//     div.classList.remove("d-none");
-//     getQuestion(0);
-// })
+let nextQuestion = 0;
+kismet.addEventListener('click', () => {
+    container.classList.remove('d-none');
+    background.classList.remove('d-none');
+    loadTest(0);
+    console.log(kismet.getAttribute("data-name"));
+})
+aski.addEventListener('click', () => {
+    container.classList.remove('d-none');
+    background.classList.remove('d-none');
+    loadTest(0);
+    console.log(aski.getAttribute("data-name"));
+})
+harryPotter.addEventListener('click', () => {
+    container.classList.remove('d-none');
+    background.classList.remove('d-none');
+    loadTest(0);
+    console.log(harryPotter.getAttribute("data-name"));
+})
 
-// const getQuestion = (i) => {
-//     const h1 = document.createElement("h1");
-//     div.appendChild(h1);
-//     h1.classList.add("text-center");
-//     h1.innerHTML = data[i].soru;
-//     const button = document.createElement("button");
-//     button.classList.add("next-btn")
-//     button.innerHTML = "next";
-//     div.appendChild(button);
-// }
 
 
-{/* <input type="radio" name="answer" id="a" class="answer" />
-<label for="a" id="a_text">Cevap...</label>   */}
+function loadTest(next) {
+    questionElement.innerHTML = data[next].soru;
+    loadAnswer(0);
+}
+
+function loadAnswer(next) {
+    a_text.innerHTML = data[next].cevaplar.a;
+    b_text.innerHTML = data[next].cevaplar.b;
+    c_text.innerHTML = data[next].cevaplar.c;
+    d_text.innerHTML = data[next].cevaplar.d;
+}
+
+submitButton.addEventListener('click', () => {
+    if(nextQuestion +1 < data.length){
+        loadTest(nextQuestion + 1);
+        loadAnswer(nextQuestion + 1);
+        nextQuestion++;
+
+    }else{
+        const finishImg = document.createElement("img");
+        const finish = document.createElement("h1");
+        const finishtest = document.createElement("h2");
+        const finishContainer = document.createElement("div");
+        // bir button ekliyorum sona gelindiğinde sayfayı yenilemek için
+        container.innerHTML="";
+        finishBtn.innerHTML = "Bitir";
+        finishBtn.classList.add("submit");
+        finishContainer.classList.add("finish-container");
+        finishContainer.appendChild(finishImg);
+        finishContainer.appendChild(finish);
+        finishContainer.appendChild(finishtest);
+        finishContainer.appendChild(finishBtn);
+        let random = Math.floor(Math.random() * result.length + 1);
+        finish.innerHTML = result[random].isim;
+        finishtest.classList.add("finish-test");
+        finishtest.innerHTML = "hellö";
+        finishImg.src = result[random].resim;
+        container.appendChild(finishContainer);
+    }
+    /* herbir elemanın (inputun içindeki class adı .answer)
+    tek tek seçili özelliğini(checked=false) kaldırdık */
+    let deneme= document.querySelectorAll(".answer");
+    deneme.forEach(element => element.checked= false);
+})
+
+    closeButton.addEventListener('click', () => {
+        container.classList.add('d-none');
+        background.classList.add('d-none');
+        nextQuestion = 0;
+    })
+
+    finishBtn.addEventListener('click', () => {
+        location.reload();
+    })
