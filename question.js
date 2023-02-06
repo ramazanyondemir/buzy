99ce626009c28cb2299318fd2746ed46c5f9336d
Merve Nur <mervenurktlmss@gmail.com>
2023-02-06 23:55:42 +0300

    test tasarımı ve soruları düzeltildi

--- question.js
+++ question.js
@@ -1,56 +1,75 @@
 const kismetseolur = [
     {
-        soru: "kısmetse olur",
+        soru: "Kaç Yaşındasın?",
         cevaplar: {
-                a: "Hakkı Bulut",
-                b: "Lana Del Rey",
-                c: "Mabel Matiz",
-                d: "Linkin Park",
+            a: "18 altı",
+            b: "18 - 24",
+            c: "25 üstü",
+            d: "Sanane",
         }
     },
     {
-        soru: "atlar uçar mıqweqe?",
+        soru: "Sevgilinde en değer verdiğin şey nedir?",
         cevaplar: {
-                a: "cevap a",
-                b: "cevap b",
-                c: "cevap c",
-                d: "cevap d",
+            a: "Kokusu",
+            b: "Parası",
+            c: "Boyu",
+            d: "Zekası",
         }
     },
     {
-        soru: "En Sevdiğin 45Müzisyen?",
+        soru: "Kaynanan senin tarzına karışsa tepkin ne olur?",
         cevaplar: {
-                a: "Sezen Aksu",
-                b: "İbrahim Tatlıses",
-                c: "Ahmet Kaya",
-                d: "Ferdi Tayfur",
+            a: "Mahkemede görüşürüz",
+            b: "Kaynanam ne derse o",
+            c: "Kaynanamın tarzını kendi tarzıma benzetirim",
+            d: "Kaynanamı evden atarım",
         }
     },
     {
-        soru: "En Sevdiğin45 Müzisyen?",
+        soru: "Bir ilişkide beklentin ne?",
         cevaplar: {
-                a: "Hakkı Bulut",
-                b: "Lana Del Rey",
-                c: "Mabel Matiz",
-                d: "Linkin Park",
+            a: "Baba gibi davranması",
+            b: "Avrupai bir bey",
+            c: "Özgüvenli olması",
+            d: "Olgun olması",
         }
     },
     {
-        soru: "En Sevdiğin Müzisye45n?",
+        soru: "Eşin çalışmanı istemedi tepkin ne olur?",
         cevaplar: {
-                a: "Hakkı Bulut",
-                b: "Lana Del Rey",
-                c: "Mabel Matiz",
-                d: "Linkin Park",
+            a: "Beyim ne derse o",
+            b: "Eski eşim",
+            c: "Ben çalışmazsam o da çalışmasın",
+            d: "Eşim getirecek ben yiyeceğim",
         }
     },
     {
-        soru: "En Sevdiğin Müzisyen4545545?",
+        soru: "Hangi ten rengi erkeklerden hoşlanırsın?",
         cevaplar: {
-                a: "Hakkı Bulut",
-                b: "Lana Del Rey",
-                c: "Mabel Matiz",
-                d: "Linkin Park",
+            a: "Sarışın",
+            b: "Esmer",
+            c: "Kumral",
+            d: "Caramionun içindeki karamel",
+        }
+    },
+    {
+        soru: "ilk buluşmada hesabı kim ödemeli?",
+        cevaplar: {
+            a: "Tabii ki de erkek bu da soru mu",
+            b: "Kim davet ettiyse o",
+            c: "Tabii ki de kadın",
+            d: "Alman usulü",
+        }
+    },
+
+    {
+        soru: "Eğer kısmetse olur evinde olsaydın hangi arkadaş grubu sana daha yakın olurdu?",
+        cevaplar: {
+            a: "Tuğçe/ Elif",
+            b: "Cansel/Aybüke",
+            c: "Yaren/Şeydanur",
+            d: " Ayşenur/Simge",
         }
     },
 ];
@@ -59,28 +78,28 @@ const askimemnu = [
     {
         soru: "askimemnu",
         cevaplar: {
-                a: "Hakkı Bulut",
-                b: "Lana Del Rey",
-                c: "Mabel Matiz",
-                d: "Linkin Park",
+            a: "Hakkı Bulut",
+            b: "Lana Del Rey",
+            c: "Mabel Matiz",
+            d: "Linkin Park",
         }
     },
     {
         soru: "atlar uçar mıqweqe?",
         cevaplar: {
-                a: "cevap a",
-                b: "cevap b",
-                c: "cevap c",
-                d: "cevap d",
+            a: "cevap a",
+            b: "cevap b",
+            c: "cevap c",
+            d: "cevap d",
         }
     },
     {
         soru: "En Sevdiğin 45Müzisyen?",
         cevaplar: {
-                a: "Sezen Aksu",
-                b: "İbrahim Tatlıses",
-                c: "Ahmet Kaya",
-                d: "Ferdi Tayfur",
+            a: "Sezen Aksu",
+            b: "İbrahim Tatlıses",
+            c: "Ahmet Kaya",
+            d: "Ferdi Tayfur",
         }
     }
 ]
@@ -90,28 +109,28 @@ const harry = [
     {
         soru: "harry potter",
         cevaplar: {
-                a: "Hakkı Bulut",
-                b: "Lana Del Rey",
-                c: "Mabel Matiz",
-                d: "Linkin Park",
+            a: "Hakkı Bulut",
+            b: "Lana Del Rey",
+            c: "Mabel Matiz",
+            d: "Linkin Park",
         }
     },
     {
         soru: "atlar uçar mıqweqe?",
         cevaplar: {
-                a: "cevap a",
-                b: "cevap b",
-                c: "cevap c",
-                d: "cevap d",
+            a: "cevap a",
+            b: "cevap b",
+            c: "cevap c",
+            d: "cevap d",
         }
     },
     {
         soru: "En Sevdiğin 45Müzisyen?",
         cevaplar: {
-                a: "Sezen Aksu",
-                b: "İbrahim Tatlıses",
-                c: "Ahmet Kaya",
-                d: "Ferdi Tayfur",
+            a: "Sezen Aksu",
+            b: "İbrahim Tatlıses",
+            c: "Ahmet Kaya",
+            d: "Ferdi Tayfur",
         }
     }
 ]
